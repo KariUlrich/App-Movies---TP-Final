44e05d2e65c83f35d0a79140cbec893e01854482
KariUlrich <kariulrich@hotmail.com>
2022-03-17 16:10:59 -0300

    mejorando imagenes y colocando mixing

--- src/componentes/Buscar.js
+++ src/componentes/Buscar.js
@@ -1,38 +1,41 @@
 import { useSearchParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import "../styles/Buscar.scss";
-import { BsSearch } from "react-icons/bs"
-
-
+import { BsSearch } from "react-icons/bs";
 
 const Buscar = () => {
+  const [searchParams, setSearchParams] = useSearchParams({ busqueda: "" });
+  const [valorDelInput, setValorDelInput] = useState("");
 
-    const [searchParams, setSearchParams] = useSearchParams({busqueda:""})
-    const [valorDelInput, setValorDelInput] = useState("")
+  const handleChange = (e) => {
+    setValorDelInput(e.target.value);
+  };
 
-    const handleChange = (e) => {
-        setValorDelInput(e.target.value)
-    }
+  const handleClick = () => {
+    setSearchParams({ busqueda: valorDelInput });
+  };
 
-    const handleClick = () => {
-        setSearchParams({busqueda: valorDelInput})
-         
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault() 
-    }
+  return (
+    <div className="formulario">
+      <form onSubmit={handleSubmit}>
+        <h3>Busca tu película</h3>
+        <input
+          className="input-peli"
+          type="text"
+          onChange={handleChange}
+          value={valorDelInput}
+        ></input>
+        <button aria-label="boton buscar" type="submit" onClick={handleClick}>
+          {" "}
+          <BsSearch />{" "}
+        </button>
+      </form>
+    </div>
+  );
+};
 
-    return(
-         <div className="formulario">
-              <form onSubmit = {handleSubmit}>
-              <h3>Busca tu película</h3>
-              <input className="input-peli" type= "text" onChange = {handleChange} value={valorDelInput}></input>
-              <button className="boton-submit" type= "submit" onClick = {handleClick}> <BsSearch /> </button>
-              </form>
-         </div>
-
-    )
-}
-
-export default Buscar
+export default Buscar;
--- src/componentes/Carousel.js
+++ src/componentes/Carousel.js
@@ -10,7 +10,7 @@ const Carousel = () => {
 
   useEffect(() => {
     fetch(
-      `https://api.themoviedb.org/3/movie/now_playing/?api_key=0f992db40ce22ab302880645bfa455bf&language=es-AR&page=1`
+      `https://api.themoviedb.org/3/movie/upcoming/?api_key=0f992db40ce22ab302880645bfa455bf&language=es-AR&page=1`
     )
       .then((res) => res.json())
       .then((data) => setPeliculas(data.results));
