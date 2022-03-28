acebc4b31275ec1114b2e5229dc2703a113e3b01
KariUlrich <kariulrich@hotmail.com>
2022-03-27 23:08:45 -0300

    armado de footer

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -1,11 +1,39 @@
-
+import { mail, linkedin, gitHub } from "../utils/variables";
+import { FiMail } from "react-icons/fi";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import "../styles/Footer.scss";
 
 const Footer = () => {
+  return (
+    <div className="contenedor-footer">
+      <div>
+        <p>App de Peliculas y Series hecho por Karina Ulrich</p>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <a href={mail}>
+              {" "}
+              <FiMail />{" "}
+            </a>
+          </li>
+          <li>
+            <a href={linkedin}>
+              {" "}
+              <FaLinkedinIn />{" "}
+            </a>
+          </li>
+          <li>
+            <a href={gitHub}>
+              {" "}
+              <FaGithub />{" "}
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-    return (
-
-        <div>Footer</div>
-    )
-}
-
-export default Footer
+export default Footer;
